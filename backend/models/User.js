1af09f3da086147b0b96c37f21f0b6b930a91abe
sathwik774444 <sathwikkulkarni123@gmail.com>
2026-01-12 16:10:46 +0530

    First commit of Fchat

---
+++ backend/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePic: { type: String, default: "" },
+  isOnline: { type: Boolean, default: false },
+  lastSeen: { type: Date }
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
